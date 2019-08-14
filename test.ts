e374b01842e82e0e5c4eb5342f17379dd26d57f1
teradonburi <daikiterai@gmail.com>
2019-08-14 09:48:59 +0900

    ts

---
+++ test.ts
@@ -0,0 +1,36 @@
+// typescript文法参考
+// https://qiita.com/uhyo/items/e2fdef2d3236b9bfe74a
+// tsconfig設定参考
+// https://qiita.com/alfas/items/539ade65926deb530e0e
+// VSCodeでESLint+@typescript-eslint+Prettierを導入する
+// https://qiita.com/madono/items/a134e904e891c5cb1d20
+
+const a = 3;
+const b = "aaaaa";
+
+interface MyObj {
+  foo: string;
+  bar: number;
+}
+
+const obj: MyObj = {
+  foo: "foo",
+  bar: 3
+};
+
+console.log(a);
+console.log(b);
+console.log(obj);
+
+const foo: number[] = [0, 1, 2, 3];
+foo.push(4);
+
+console.log(foo);
+
+function func(arg: string): number {
+  return Number(arg);
+}
+
+const f: (foo: string) => number = func;
+
+console.log(f("1"));
