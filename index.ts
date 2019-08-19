964b5dc1c33813ee6f36764bf2db699823deb80f
teradonburi <daikiterai@gmail.com>
2019-08-19 09:46:41 +0900

    null check

--- index.ts
+++ index.ts
@@ -1,30 +1,44 @@
+
+// string, number, boolean
 const a: number = 3;
-const b: string = "aaaaa";
-
-// Objectの型定義にはinterfaceを使う 
-interface MyObj {
-  foo: string;
-  bar: number;
-}
-
-const obj: MyObj = {
-  foo: "foo",
-  bar: 3
-};
+const b: string = "hello world!";
+const c: boolean = true;
 
 console.log(a);
 console.log(b);
-console.log(obj);
+console.log(c);
 
-const foo: number[] = [0, 1, 2, 3];
-foo.push(4);
 
-console.log(foo);
+// array, object, enum, never
+// any
+// void
+// 相互に代入不可
 
-function func(arg: string): number {
-  return Number(arg);
-}
 
-const f: (foo: string) => number = func;
+// // Objectの型定義にはinterfaceを使う 
+// interface MyObj {
+//   foo: string;
+//   bar: number;
+// }
 
-console.log(f("1"));
+// const obj: MyObj = {
+//   foo: "foo",
+//   bar: 3
+// };
+
+// console.log(a);
+// console.log(b);
+// console.log(obj);
+
+// const foo: number[] = [0, 1, 2, 3];
+// foo.push(4);
+
+// console.log(foo);
+
+// function func(arg: string): number {
+//   return Number(arg);
+// }
+
+// const f: (foo: string) => number = func;
+
+// console.log(f("1"));
